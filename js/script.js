af34fcba5610d75d12b357472e852dcbd82eb1ae
rodeomar <raed.alkhanbashi@gmail.com>
2023-03-27 15:30:12 -0700

    add arrays, loops, and reverse the order of the new array
    Co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 window.addEventListener("load", function() {
 document.querySelector("form").addEventListener("submit", userInput)
 })
+
 function userInput(e){
     e.preventDefault()
     const sentence = document.querySelector("#sentence").value 
-    const newWords = sentence.split()
+    const newWords = sentence.split(" ") //new array from the sentence
+    let finalWordPlay = newsentences(newWords)
     
-    console.log(newWords);
+    const response = document.createElement("p");
+    response.append(finalWordPlay.join(" "))
+    document.body.append(response)
 }
 
-
+function newsentences(words) {
+    const sentence = words.filter(word => word.length >= 3);
+    return sentence.reverse()
+}
